8867350eb73e272cef7e2fd526fcff7d9138d505
fredrikj31 <fredrik1206@gmail.com>
2019-11-15 13:21:01 +0100

    Added gui to new equation

--- MathCompleter/AndengradsLigning.xaml.cs
+++ MathCompleter/AndengradsLigning.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -29,7 +30,8 @@ namespace MathCompleter
 		}
         private void Udreng_Click(object sender, RoutedEventArgs e)
         {
-            try_Loop = 0;
+			/* OLD CODE
+			try_Loop = 0;
             Output_AndenGradsLigning.Inlines.Clear();
             while (try_Loop < 1){
                 // Try catch to detect non numeric values and stop running remainder of script
@@ -72,8 +74,66 @@ namespace MathCompleter
                     }
                 }
                 try_Loop +=1;
-            }
-        }
+            }*/
+
+			Output_AndenGradsLigning.Inlines.Clear();
+
+			if (Ligning.Text == "Indtast ligning her Eksempel: 1x^2+2x+3=0")
+			{
+				MessageBox.Show("Please type something in the text field");
+			} else
+			{
+				char[] delimiterChars = { ' ', '=', '^', 'x', 'X', '+' };
+				string Bruh = Ligning.Text;
+				ArrayList ar = new ArrayList();
+
+				string[] tests = Bruh.Split(delimiterChars);
+
+				//Mangler stadig et for loop
+				foreach (var test in tests)
+				{
+					if (test == "")
+					{
+						continue;
+					}
+					else
+					{
+						ar.Add(test);
+					}
+				}
+
+				int c = ar.Count;
+				double Ligmed = Convert.ToDouble(ar[c - 1]);
+				double Ligmed1 = Convert.ToDouble(ar[c - 2]);
+				double A = Convert.ToDouble(ar[2]);
+				double B = Convert.ToDouble(ar[0]);
+				double C = Ligmed1 - Ligmed;
+				double D = Math.Pow(B, 2) - 4 * A * C;
+				Output_AndenGradsLigning.Inlines.Add("Discriminanten: " + D + "\n");
+				double Løsning1 = -B + Math.Sqrt(D);
+				double Løsning2 = -B - Math.Sqrt(D);
+				if (D > 0)
+				{
+					Output_AndenGradsLigning.Inlines.Add("Plus Løsning: " + Løsning1 + "\nMinus Løsning:" + Løsning2);
+				}
+				if (D < 0)
+				{
+					Output_AndenGradsLigning.Inlines.Add("Ingen Maginær Løsning");
+				}
+				if (D == 0)
+				{
+					if (Løsning1 != 0)
+					{
+						Output_AndenGradsLigning.Inlines.Add("Den eneste Løsning er plus løsning som er " + Løsning1);
+					}
+					else
+					{
+						Output_AndenGradsLigning.Inlines.Add("Den eneste løsning er minus løsningen som er " + Løsning2);
+					}
+				}
+			}
+
+		}
 
 
     }
