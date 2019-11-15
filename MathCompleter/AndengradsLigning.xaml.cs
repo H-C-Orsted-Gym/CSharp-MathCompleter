91aa76eef8c02d065069bcf366692db334e40182
fredrikj31 <fredrik1206@gmail.com>
2019-11-15 13:22:38 +0100

    Removed old code

--- MathCompleter/AndengradsLigning.xaml.cs
+++ MathCompleter/AndengradsLigning.xaml.cs
@@ -30,52 +30,6 @@ namespace MathCompleter
 		}
         private void Udreng_Click(object sender, RoutedEventArgs e)
         {
-			/* OLD CODE
-			try_Loop = 0;
-            Output_AndenGradsLigning.Inlines.Clear();
-            while (try_Loop < 1){
-                // Try catch to detect non numeric values and stop running remainder of script
-                try
-                {
-                    aVærdi = Convert.ToDouble(Aværdi.Text);
-                    bVærdi = Convert.ToDouble(Bværdi.Text);
-                    cVærdi = Convert.ToDouble(Cværdi.Text);
-                }
-                catch (Exception)
-                {
-                    Output_AndenGradsLigning.Inlines.Add("Non value input");
-                    break;
-                }
-                
-            //calculating Discriminant and answers 1 and 2
-            Discriminant = Math.Pow(bVærdi,2) - 4*(aVærdi * cVærdi);
-            double Svar1_2GradsLigning = (-bVærdi + Math.Sqrt(Discriminant)) / (2 * aVærdi);
-            double Svar2_2GradsLigning = (-bVærdi - Math.Sqrt(Discriminant)) / (2 * aVærdi);
-            Output_AndenGradsLigning.Inlines.Add("Ligningen: " + aVærdi + "x^2 + " + bVærdi + "x +" + cVærdi + "\nDiscriminanten: " + Discriminant);
-            
-            //figuring out what to output
-            if (Discriminant < 0)
-            {
-                Output_AndenGradsLigning.Inlines.Add("\nIngen Løsning");
-            }
-            if (Discriminant > 0)
-                {
-                    Output_AndenGradsLigning.Inlines.Add("\nLøsning 1: " + Svar1_2GradsLigning + "\nLøsning 2: " + Svar2_2GradsLigning);
-                }
-            if (Discriminant == 0)
-                {
-                    if (Svar1_2GradsLigning == 0)
-                    {
-                        Output_AndenGradsLigning.Inlines.Add("\nEneste Løsning: " + Svar2_2GradsLigning);
-                    }
-                    else
-                    {
-                        Output_AndenGradsLigning.Inlines.Add("\nEneste Løsning: " + Svar1_2GradsLigning);
-                    }
-                }
-                try_Loop +=1;
-            }*/
-
 			Output_AndenGradsLigning.Inlines.Clear();
 
 			if (Ligning.Text == "Indtast ligning her Eksempel: 1x^2+2x+3=0")
